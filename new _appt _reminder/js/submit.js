2efc25e754bd04d31224118a9cf131a2e879caaf
yhuang306 <56890332+yhuang306@users.noreply.github.com>
2022-07-17 18:23:34 -0700

    Update submit.js

--- new _appt _reminder/js/submit.js
+++ new _appt _reminder/js/submit.js
@@ -4,20 +4,20 @@ function submit() {
 
     if (remind_type === 'appt_remind'){
         if (appt_type === 'lab')
-            location.href = 'yhuang306.github.io/new _appt _reminder/templates/lab.html';
+            window.location.href = 'yhuang306.github.io/new _appt _reminder/templates/lab.html';
         else if (appt_type === 'f/u')
-            location.href = 'yhuang306.github.io/new _appt _reminder/templates/followup.html';
+            window.location.href = 'yhuang306.github.io/new _appt _reminder/templates/followup.html';
         else if (appt_type === 'shingrix')
-            location.href = 'yhuang306.github.io/new _appt _reminder/templates/shingrix.html';
+            window.location.href = 'yhuang306.github.io/new _appt _reminder/templates/shingrix.html';
         else
-            location.href = 'yhuang306.github.io/new _appt _reminder/templates/labAndShingrix.html';
+            window.location.href = 'yhuang306.github.io/new _appt _reminder/templates/labAndShingrix.html';
     }
 
     else if (remind_type === 'report'){
-        location.href = 'yhuang306.github.io/new _appt _reminder/templates/report.html';
+        window.location.href = 'yhuang306.github.io/new _appt _reminder/templates/report.html';
     }
     else{
-        location.href = "result.html";
+        window.location.href = "result.html";
     }
     
 } 
